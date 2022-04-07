8d6b310f0cc619008c2dd890e428d666146dc22d
JAnderson217 <24385176+JAnderson217@users.noreply.github.com>
2022-04-07 10:43:59 +0100

    C# basic OOP exercises - Inheritance

---
+++ Person.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    class Person
+    {
+        public Person() { }
+        public String firstName { get; set; }
+        public String lastName { get; set; }
+        public int age { get; set; }
+        public double currentAccount { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inheritance
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Instantiate Account 
+            Account myAccount = new Account();
+            double currentAmount = 0.0;
+            //save money in current account
+            currentAmount = myAccount.setAccountBalance(1000.00);
+            Console.WriteLine($"current amount: {currentAmount}");
+            //withdraw 200.00
+            currentAmount = myAccount.withdrawFromAccount(200.00);
+            Console.WriteLine($"current amount: {currentAmount}");
+            //get balance
+            currentAmount = myAccount.getAccountBalance();
+            Console.WriteLine($"current amount: {currentAmount}");
+            Console.ReadLine();
+
+        }
+    }
+}
