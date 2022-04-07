ee612475c9680a2d68954e665af14854c23cc56e
JAnderson217 <24385176+JAnderson217@users.noreply.github.com>
2022-04-07 12:35:31 +0100

    Overriding

---
+++ Overriding/Overriding/AreaOfShapes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Overriding
+{
+    internal class AreaOfShapes:AreaOfSquare
+    {
+        public double Rectangle(double a, double b)
+        {
+            return a * b;
+        }
+        //Override square method in AreaOfSquare
+        public override double Square(double a, double b)
+        {
+            //return area of square with a*b, was a^b previously
+            Console.WriteLine("Area of shapes called square a*b");
+            return a*b;
+        }
+
+    }
+}
